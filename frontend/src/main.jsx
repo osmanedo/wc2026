f00d6e82524639b3bf925a44ef0f6c77b57bfb93
Eduardo Perez <osmanedo@gmail.com.com>
2026-04-10 15:05:43 +1000

    updated vercel analytics

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,8 +4,6 @@ import { Analytics } from "@vercel/analytics/react"
 import './index.css'
 import App from './App.jsx'
 
-inject()
-
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
